7093d932f50b2e0cbed08659437deee631c15185
jianbingfang <jianbingfang@gmail.com>
2014-08-13 17:05:19 +0800

    creation

---
+++ src/com/hubahuma/mobile/WritingActivity.java
@@ -0,0 +1,15 @@
+package com.hubahuma.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WritingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_writing);
+	}
+}
