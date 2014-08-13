df2b2801696a797f6850a0703265e72b993000cb
jianbingfang <jianbingfang@gmail.com>
2014-08-13 19:37:13 +0800

    add

---
+++ src/com/hubahuma/mobile/news/MessageActivity.java
@@ -0,0 +1,18 @@
+package com.hubahuma.mobile.news;
+
+import com.hubahuma.mobile.R;
+import com.hubahuma.mobile.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_message);
+	}
+}
