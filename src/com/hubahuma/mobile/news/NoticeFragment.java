78f002017da8c96a7a86f80cd34bcbb386347905
jianbingfang <jianbingfang@gmail.com>
2014-08-15 15:55:37 +0800

    pull to refresh test

--- src/com/hubahuma/mobile/news/NoticeFragment.java
+++ src/com/hubahuma/mobile/news/NoticeFragment.java
@@ -1,27 +1,50 @@
 package com.hubahuma.mobile.news;
 
-import org.androidannotations.annotations.AfterInject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
+import org.json.JSONArray;
 
-import com.hubahuma.mobile.R;
-
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.hubahuma.mobile.R;
 
 @EFragment(R.layout.fragment_notice)
 public class NoticeFragment extends BaseFragment {
 
 	private boolean isInit; // 是否可以开始加载数据
 
-	@ViewById
-	ListView list;
+	private ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+
+	@ViewById(R.id.pull_refresh_list)
+	PullToRefreshListView mPullRefreshListView;
+
+	MyAdapter adapter = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,12 +55,84 @@ public class NoticeFragment extends BaseFragment {
 	@AfterViews
 	void init() {
 		isInit = true;
+		// 设定下拉监听函数
+		mPullRefreshListView
+				.setOnRefreshListener(new OnRefreshListener<ListView>() {
+					@Override
+					public void onRefresh(
+							PullToRefreshBase<ListView> refreshView) {
+						String label = DateUtils.formatDateTime(
+								NoticeFragment.this.getActivity()
+										.getApplicationContext(), System
+										.currentTimeMillis(),
+								DateUtils.FORMAT_SHOW_TIME
+										| DateUtils.FORMAT_SHOW_DATE
+										| DateUtils.FORMAT_ABBREV_ALL);
+
+						// Update the LastUpdatedLabel
+						refreshView.getLoadingLayoutProxy()
+								.setLastUpdatedLabel(label);
+
+						// Do work to refresh the list here.
+						new GetDataTask().execute();
+					}
+				});
+
+		mPullRefreshListView.setMode(Mode.PULL_FROM_START);// 设置底部下拉刷新模式
+
+		listItem = getData();// 获取LIST数据
+		adapter = new MyAdapter(this.getActivity().getApplicationContext());
+
+		// 设置适配器
+		ListView actualListView = mPullRefreshListView.getRefreshableView();
+		actualListView.setAdapter(adapter);
 	}
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		/* 初始化控件 */
+	private class GetDataTask extends
+			AsyncTask<Void, Void, HashMap<String, Object>> {
+
+		// 后台处理部分
+		@Override
+		protected HashMap<String, Object> doInBackground(Void... params) {
+			// Simulates a background job.
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			try {
+
+				map = new HashMap<String, Object>();
+				map.put("name", "林珊");
+				map.put("info", "上传了一张新照片油画");
+				map.put("img", "youhua");
+
+			} catch (Exception e) {
+				NoticeFragment.this.getActivity().setTitle("map出错了");
+				return null;
+			}
+
+			return map;
+		}
+
+		// 这里是对刷新的响应，可以利用addFirst（）和addLast()函数将新加的内容加到LISTView中
+		// 根据AsyncTask的原理，onPostExecute里的result的值就是doInBackground()的返回值
+		@Override
+		protected void onPostExecute(HashMap<String, Object> result) {
+			// 在头部增加新添内容
+
+			try {
+				listItem.add(result);
+				// 通知程序数据集已经改变，如果不做通知，那么将不会刷新mListItems的集合
+				adapter.notifyDataSetChanged();
+				// Call onRefreshComplete when the list has been refreshed.
+				mPullRefreshListView.onRefreshComplete();
+			} catch (Exception e) {
+				NoticeFragment.this.getActivity().setTitle(e.getMessage());
+			}
+
+			super.onPostExecute(result);
+		}
 	}
 
 	@Override
@@ -75,26 +170,149 @@ public class NoticeFragment extends BaseFragment {
 
 	}
 
-	/*
+	private ArrayList<HashMap<String, Object>> getData() {
+		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		InputStream inputStream;
+		try {
+			inputStream = this.getActivity().getAssets()
+					.open("notice_items.txt");
+			String json = readTextFile(inputStream);
+			JSONArray array = new JSONArray(json);
+			for (int i = 0; i < array.length(); i++) {
+				map = new HashMap<String, Object>();
+				map.put("portrait", array.getJSONObject(i)
+						.getString("portrait"));
+				map.put("author_name",
+						array.getJSONObject(i).getString("author_name"));
+				map.put("content_img",
+						array.getJSONObject(i).getString("content_img"));
+				map.put("content_txt",
+						array.getJSONObject(i).getString("content_txt"));
+				map.put("message_date",
+						array.getJSONObject(i).getString("message_date"));
+				list.add(map);
+			}
+			return list;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	@EBean
+	public class ViewHolder {
+		@ViewById
+		ImageButton portrait;
+		@ViewById
+		ImageView content_img;
+		@ViewById
+		TextView author_name, content_txt, message_date;
+	}
+
+	public class MyAdapter extends BaseAdapter {
+
+		private LayoutInflater mInflater;
+
+		public MyAdapter(Context context) {
+			this.mInflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public int getCount() {
+			return listItem.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			ViewHolder holder = null;
+			if (convertView == null) {
+
+				convertView = mInflater.inflate(R.layout.msglist_item_notice,
+						null);
+				holder = new ViewHolder();
+				// holder.img = (ImageView) convertView.findViewById(R.id.img);
+				// holder.name = (TextView) convertView.findViewById(R.id.name);
+				// holder.info = (TextView) convertView.findViewById(R.id.info);
+				convertView.setTag(holder);
+
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+
+			// TODO 判断真实头像
+			holder.portrait.setImageResource(R.drawable.default_portrait);
+			// TODO 判断是否真的有图片
+			holder.content_img.setVisibility(View.GONE);
+			;
+
+			holder.author_name.setText((String) listItem.get(position).get(
+					"name"));
+			holder.content_txt.setText((String) listItem.get(position).get(
+					"content_txt"));
+			holder.message_date.setText((String) listItem.get(position).get(
+					"message_date"));
+
+			return convertView;
+		}
+	}
+
+	public String readTextFile(InputStream inputStream) {
+		String readedStr = "";
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+			String tmp;
+			while ((tmp = br.readLine()) != null) {
+				readedStr += tmp;
+			}
+			br.close();
+			inputStream.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return readedStr;
+	}
+
+	/**********************************************************************************
 	 * private boolean isInit; // 是否可以开始加载数据
 	 * 
-	 * @AfterViews void init() { isInit = true; show(); }
-	 * 
+	 * @ViewById ListView list;
+	 * @Override public View onCreateView(LayoutInflater inflater, ViewGroup
+	 *           container, Bundle savedInstanceState) { return null; }
+	 * @AfterViews void init() { isInit = true; }
+	 * @Override public void onViewCreated(View view, Bundle savedInstanceState)
+	 *           { super.onViewCreated(view, savedInstanceState); // 初始化控件 }
 	 * @Override public void onResume() { super.onResume(); // 判断当前fragment是否显示
-	 * if (getUserVisibleHint()) {
-	 * Toast.makeText(getActivity().getApplicationContext(), "notice resume",
-	 * Toast.LENGTH_SHORT); showData(); } }
-	 * 
+	 *           if (getUserVisibleHint()) { showData();
+	 *           Toast.makeText(getActivity().getApplicationContext(),
+	 *           "notice resume", Toast.LENGTH_SHORT).show(); ; } }
 	 * @Override public void setUserVisibleHint(boolean isVisibleToUser) {
-	 * super.setUserVisibleHint(isVisibleToUser); // 每次切换fragment时调用的方法 if
-	 * (isVisibleToUser) { Toast.makeText(getActivity().getApplicationContext(),
-	 * "notice", Toast.LENGTH_SHORT); showData(); } }
+	 *           super.setUserVisibleHint(isVisibleToUser); //
+	 *           每次切换fragment时调用的方法 if (isVisibleToUser) { showData(); //
+	 *           Toast.makeText(getActivity().getApplicationContext(), "notice",
+	 *           // Toast.LENGTH_SHORT).show(); } }
 	 * 
-	 * private void showData() { if (isInit) { isInit = false;// 加载数据完成 //
-	 * 加载各种数据 } }
+	 *           private void showData() { if (isInit) { isInit = false;//
+	 *           加载数据完成 // 加载各种数据 } }
+	 * @Override public void show() {
 	 * 
-	 * @Override public void show() { // TODO Auto-generated method stub
-	 * 
-	 * }
-	 */
+	 *           }
+	 *************************************************************************************/
 }
