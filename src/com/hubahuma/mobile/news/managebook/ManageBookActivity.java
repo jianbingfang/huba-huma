326073bfe4b4b4a9609767b3677de59368dd6aac
jianbingfang <jianbingfang@gmail.com>
2014-08-17 21:33:06 +0800

    update

---
+++ src/com/hubahuma/mobile/news/managebook/ManageBookActivity.java
@@ -0,0 +1,45 @@
+package com.hubahuma.mobile.news.managebook;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.NoTitle;
+
+import com.hubahuma.mobile.R;
+import com.hubahuma.mobile.R.layout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+@SuppressWarnings("deprecation")
+@NoTitle
+@EActivity(R.layout.activity_manage_book)
+public class ManageBookActivity extends Activity {
+
+	@AfterViews
+	void init(){
+		
+	}
+	
+	@Click
+	void btn_group_manage(){
+		// TODO 
+	}
+	
+	@Click
+	void btn_back() {
+		Intent intent = getIntent();
+		intent.putExtra("result", "returned from ManageBookActivity");
+		this.setResult(0, intent);
+		this.finish();
+	}
+
+	@Override
+	public void onBackPressed() {
+		btn_back();
+	}
+	
+}
