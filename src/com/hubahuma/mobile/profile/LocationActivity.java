1e4d7ad804b43139847b881d37bd0afc5f3ac880
jianbingfang <jianbingfang@gmail.com>
2014-09-03 01:57:48 +0800

    add baidu map

--- src/com/hubahuma/mobile/profile/LocationActivity.java
+++ src/com/hubahuma/mobile/profile/LocationActivity.java
@@ -1,8 +1,11 @@
 package com.hubahuma.mobile.profile;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.NoTitle;
+import org.androidannotations.annotations.ViewById;
 
 import com.hubahuma.mobile.R;
 import com.hubahuma.mobile.R.layout;
@@ -12,13 +15,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 @SuppressWarnings("deprecation")
 @NoTitle
 @EActivity(R.layout.activity_location)
 public class LocationActivity extends Activity {
 
+	@ViewById
+	TextView org_name, org_addr, org_phone;
 	
+	@Extra
+	String name, address, phone;
+	
+	@AfterViews
+	void init(){
+		org_name.setText(name);
+		org_addr.setText(address);
+		org_phone.setText(phone);
+	}
 	
 	@Click
 	void btn_back() {
